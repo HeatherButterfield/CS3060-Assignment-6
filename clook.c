5f6e551f31961e959e007a4a5833342294a78128
Heather H <Z_10661215@cs3060.ad.uvu.edu>
2018-11-28 15:23:35 -0700

    commit

--- clook.c
+++ clook.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 void fcfs(int blockList[], int count) {
-        printf("In CLOOK:\n");
-        for (int i = 0; i < count; i++) {
-                printf("%d ", blockList[i]);
-        }
-        print("\n");
+  printf("In CLOOK:\n");
+  for (int i = 0; i < count; i++) {
+    printf("%d ", blockList[i]);
+  }
+  printf("\n");
 }
--- look.c
+++ look.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 void fcfs(int blockList[], int count) {
-        printf("In LOOK:\n");
-        for (int i = 0; i < count; i++) {
-                printf("%d ", blockList[i]);
-        }
-        print("\n");
+  printf("In LOOK:\n");
+  for (int i = 0; i < count; i++) {
+    printf("%d ", blockList[i]);
+  }
+  printf("\n");
 }
