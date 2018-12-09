94bc2bb8c10d71a275eeb958edd8b38df9a6b1b5
Mariah Bleak <33531667+mlbleak@users.noreply.github.com>
2018-12-08 22:35:07 -0700

    Update and rename clook.c to clook.cpp

--- clook.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void clook(int blockList[], int count) {
-	printf("clook works!\n");
-}
---
+++ clook.cpp
@@ -0,0 +1,14 @@
+#include <stdio>
+#include <iomanip>
+
+using namespace std;
+
+void clook(int blockList[], int count) {
+	printf("clook works!\n");
+}
+
+int main()
+{
+
+return 0;
+}
